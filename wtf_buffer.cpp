f8e367f635c568f914e613c4eacdb3a98ca9a519
parihaaraka <parihaaraka@gmail.com>
2025-03-11 18:30:55 +0300

    wtf_buffer adjusted

--- wtf_buffer.cpp
+++ wtf_buffer.cpp
@@ -1,79 +1,99 @@
 #include "wtf_buffer.h"
 
-wtf_buffer::wtf_buffer(size_t size) : _buf(size)
+wtf_buffer::wtf_buffer(size_t size) : _local_buf(size)
 {
-    end = _buf.data();
+    _buf = &_local_buf;
+    end = _buf->data();
 }
 
-wtf_buffer::wtf_buffer(std::vector<char> buf) : _buf(std::move(buf))
+wtf_buffer::wtf_buffer(std::vector<char> &buf, size_t offset) : _buf(&buf)
 {
-    end = _buf.data();
+    end = _buf->data() + offset;
+}
+
+wtf_buffer::wtf_buffer(std::vector<char> &&buf, size_t offset) : _local_buf(std::move(buf))
+{
+    _buf = &_local_buf;
+    end = _buf->data() + offset;
 }
 
 size_t wtf_buffer::capacity() const noexcept
 {
-    return _buf.size();
+    return _buf->size();
 }
 
 size_t wtf_buffer::size() const noexcept
 {
-    return static_cast<size_t>(end - _buf.data());
+    return static_cast<size_t>(end - _buf->data());
 }
 
 size_t wtf_buffer::available() const noexcept
 {
-    return _buf.size() - size();
+    return _buf->size() - size();
 }
 
 char *wtf_buffer::data() noexcept
 {
-    return _buf.data();
+    return _buf->data();
 }
 
 const char *wtf_buffer::data() const noexcept
 {
-    return _buf.data();
+    return _buf->data();
 }
 
 void wtf_buffer::reserve(size_t size)
 {
-    size_t s = this->size();
-    _buf.resize(size);
-    end = _buf.data() + s;
+	size_t s = this->size();
+    _buf->resize(size);
+    end = _buf->data() + s;
 }
 
 void wtf_buffer::resize(size_t size)
 {
     if (size > capacity())
-        _buf.resize(size);
-    end = _buf.data() + size;
+        _buf->resize(size);
+    end = _buf->data() + size;
 }
 
 void wtf_buffer::clear() noexcept
 {
-    end = _buf.data();
+    end = _buf->data();
     if (on_clear)
         on_clear();
 }
 
 void wtf_buffer::swap(wtf_buffer &other) noexcept
 {
-    _buf.swap(other._buf);
-    std::swap(end, other.end);
+    bool this_owner = (_buf == &_local_buf);
+	size_t this_size = size();
+    bool other_owner = (other._buf == &other._local_buf);
+    size_t other_size = other.size();
+    _local_buf.swap(other._local_buf);
+    std::swap(_buf, other._buf);
+    if (this_owner)
+		other._buf = &other._local_buf;
+	if (other_owner)
+		_buf = &_local_buf;
+	end = _buf->data() + other_size;
+	other.end = other.data() + this_size;
     // DO NOT swap on_clear!
 }
 
-wtf_buffer::wtf_buffer(wtf_buffer &&src) : end(src.end), _buf(std::move(src._buf))
+wtf_buffer::wtf_buffer(wtf_buffer &&src)
 {
-    // end stays valid
-    //_buf.swap(src._buf);
+    _local_buf = std::move(src._local_buf);
+    _buf = src._buf;
+    end = src.end;
 }
 
 wtf_buffer &wtf_buffer::operator=(wtf_buffer &&src)
 {
-    // end stays valid
     if (this != &src)
-        _buf.swap(src._buf);
+    {
+        _local_buf = std::move(src._local_buf);
+        _buf = src._buf;
+        end = src.end;
+    }
     return *this;
 }
-
--- wtf_buffer.h
+++ wtf_buffer.h
@@ -9,7 +9,11 @@ class wtf_buffer
 {
 public:
     explicit wtf_buffer(size_t size = 1024 * 1024);
-    wtf_buffer(std::vector<char> buf);
+    /// construct over existing vector, non-owning mode
+    wtf_buffer(std::vector<char> &buf, size_t offset = 0);
+    /// construct over existing vector, take ownership
+    wtf_buffer(std::vector<char> &&buf, size_t offset = 0);
+
     size_t capacity() const noexcept;
     size_t size() const noexcept;
     size_t available() const noexcept;
@@ -28,7 +32,8 @@ public:
     wtf_buffer(const wtf_buffer &) = delete;
     wtf_buffer& operator= (const wtf_buffer &) = delete;
 private:
-    std::vector<char> _buf;
+    std::vector<char> _local_buf;
+    std::vector<char> *_buf; // avoid reference to make swap() work
 };
 
 #endif // WTF_BUFFER_H
